f94871a17b1af56c66f22f92a8c27af0e870f262
Andrew Lalaev <andrey.lalaev@gmail.com>
2024-01-16 08:21:50 +0100

    emacs: init.el: let's give it a chance

---
+++ emacs/init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; install "use-package" if not installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file t)
+
+(use-package vscode-dark-plus-theme
+  :ensure t
+  :config
+  (load-theme 'vscode-dark-plus t))
+
+(use-package vimrc-mode)
+(use-package magit)
