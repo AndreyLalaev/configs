43d6b306f85179979441779b447a64c4bf73857c
Andrei Lalaev <andrey.lalaev@gmail.com>
2026-07-18 13:27:22 +0200

    nvim: init.lua: map Oil to E

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,6 @@ require("config.lazy")
 
 vim.wo.number=true
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohl<cr>", { desc = "Turn off search highlight" })
+vim.api.nvim_create_user_command('E', function(opts)
+  vim.cmd('Oil ' .. opts.args)
+end, { nargs = '*', desc = 'Open Oil file explorer' })
