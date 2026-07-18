6c7b2cbd0bfb2bf07025d135b5635e4885d2886d
Andrei Lalaev <andrey.lalaev@gmail.com>
2026-07-18 14:08:30 +0200

    nvim: init: highlight cursor line

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require("config.lazy")
 
-vim.wo.number=true
+vim.wo.number = true
+vim.wo.cursorline = true
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohl<cr>", { desc = "Turn off search highlight" })
 vim.api.nvim_create_user_command('E', function(opts)
   vim.cmd('Oil ' .. opts.args)
