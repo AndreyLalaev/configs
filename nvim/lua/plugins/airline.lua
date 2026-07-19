65c4367c0fa9fc8cef9cf1df40f061c3251c24f8
Andrei Lalaev <andrey.lalaev@gmail.com>
2026-07-19 12:54:47 +0200

    nvim: airline: use lualine

--- nvim/lua/plugins/airline.lua
+++ nvim/lua/plugins/airline.lua
@@ -1,49 +1,38 @@
 return {
   {
-    "vim-airline/vim-airline",
-    config = function()
-      vim.g["airline#extensions#tabline#enabled"] = 1
-      vim.g["airline#extensions#whitespace#enabled"] = 0
-      vim.g.airline_powerline_fonts = 1
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      theme = 'vscode',
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', {'fileformat', icons_enabled = false}, 'filetype'},
+        lualine_y = {},
+        lualine_z = {"%l/%L:%v"}
+      },
 
-      -- unicode
-      vim.g.airline_left_sep = '»'
-      vim.g.airline_left_sep = '▶'
-      vim.g.airline_right_sep = '«'
-      vim.g.airline_right_sep = '◀'
-      vim.g.airline_symbols.linenr = '␊'
-      vim.g.airline_symbols.linenr = '␤'
-      vim.g.airline_symbols.linenr = '¶'
-      vim.g.airline_symbols.branch = '⎇'
-      vim.g.airline_symbols.paste = 'ρ'
-      vim.g.airline_symbols.paste = 'Þ'
-      vim.g.airline_symbols.paste = '∥'
-      vim.g.airline_symbols.whitespace = 'Ξ'
-
-      -- airline symbols
-      vim.g.airline_left_sep = ''
-      vim.g.airline_left_alt_sep = ''
-      vim.g.airline_right_sep = ''
-      vim.g.airline_right_alt_sep = ''
-      vim.g.airline_symbols.branch = ''
-      vim.g.airline_symbols.readonly = ''
-      vim.g.airline_symbols.linenr = ''
-
-      -- Define the custom airline part
-      vim.fn['airline#parts#define']('line-info', {
-        raw = '%l/%L:%v',
-        accent = 'bold'
-      })
-
-      -- Assign the created section to the global airline_section_z variable
-      vim.g.airline_section_z = vim.fn['airline#section#create']({ 'line-info' })
-
-      vim.g["airline#extensions#tabline#buffer_nr_show"] = 1
-      vim.g["airline#extensions#tabline#formatter"] = 'unique_tail'
-    end,
-  },
-
-  {
-    "vim-airline/vim-airline-themes"
-  },
+      tabline = {
+          lualine_a = {''},
+          lualine_b = {''},
+          lualine_c = {
+            {
+              'buffers',
+              mode = 4, -- Shows buffer name + buffer number
+              buffers_color = {
+                active = 'lualine_a_normal',
+                inactive = function(section)
+                  local c = require('vscode.colors').get_colors()
+                  return {c.vscGray}
+                end,
+              },
+            }
+          },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {''}
+      },
+    }
+  }
 }
